53db2832820e3550393616b8a3f7fa033274d1ea
Gavli Mayuri <mrgavli9@gmail.com>
2024-12-16 22:59:26 +0530

    Hot fix

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
+# from setuptools import setup, find_packages
+# from typing import List
+
+# hypen = "-e ."
+# def get_requirements(file_path: str) -> List[str]:
+#     requirements=[]
+#     with open(file_path) as file:
+#         requirements=file.readlines()
+#         requirements = [line.strip() for line in requirements ]
+#         if hypen in requirements:
+#             requirements.remove(hypen)
+#     return requirements
+
+# setup(
+#     name="mlproject",
+#     version="0.0.1",
+#     description="This is MR's first ML project.",
+#     author="MR",
+#     author_email="mrgavli9@gmail.com",
+#     packages=find_packages(),  # Automatically find sub-packages
+#     install_requires=get_requirements("requirements.txt"),
+# )
+
+
 from setuptools import setup, find_packages
 from typing import List
 
-hypen = "-e ."
+# Removed redundant hypen, made the function more generic.
 def get_requirements(file_path: str) -> List[str]:
-    requirements=[]
+    requirements = []
     with open(file_path) as file:
-        requirements=file.readlines()
-        requirements = [line.s trip() for line in requirements ]
-        if hypen in requirements:
-            requirements.remove(hypen)
+        requirements = file.readlines()
+        requirements = [line.strip() for line in requirements ]  # Remove empty lines
+        if "-e ." in requirements:
+            requirements.remove("-e .")  # Remove the local editable install
     return requirements
 
 setup(
--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error_msg,error_detail:sys):       #error_msg: This is a string that represents the actual error message , error_detail: sys: This expects the sys module
     _,_,exc_tb = error_detail.exc_info() #To retrieve the details of current exception = sys.exc_info() , first and second is type and value which is not required to us. 
-    file_name = exc_tb.exc_frame.f_code.co_file_name # Refers to the current stack frame where the exception occurred. f_code.co_filename: Retrieves the name of the file in which the error occurred.
+    file_name = exc_tb.tb_frame.f_code.co_filename # Refers to the current stack frame where the exception occurred. f_code.co_filename: Retrieves the name of the file in which the error occurred.
     error_msg = f"Error script name {file_name} line number {exc_tb.tb_lineno} and error message {str(error_msg)} " # exc_tb.tb_lineno: Retrieves the line number in the file where the exception occurred.
 
     return error_msg
